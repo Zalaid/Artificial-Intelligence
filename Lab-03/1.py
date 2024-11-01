53ef918d41425443f57882946b06c5ea15c45bc0
Zalaid <zalaidbutt1@gmail.com>
2024-11-02 00:48:56 +0500

    lab3 comments

--- Lab-03/1.py
+++ Lab-03/1.py
@@ -1,10 +1,10 @@
-import heapq  
+import heapq  # to get minimum value from queue
 
 class Node:  
     def __init__(self, state, parent, move, cost_g, cost_h):  
-        self.state = state  
-        self.parent = parent  
+        self.state = state    #current state
+        self.parent = parent   
         self.move = move  
         self.cost_g = cost_g  
         self.cost_h = cost_h  
-        self.cost_f = cost_g + cost_h  
+        self.cost_f = cost_g + cost_h    #heusristic function
