16cfd8813fd8dac24918ab4ba9676f2930255163
Zalaid <zalaidbutt1@gmail.com>
2024-11-05 23:07:13 +0500

    lab4-minimax code completed

--- Lab-05/minimax.py
+++ Lab-05/minimax.py
@@ -41,3 +41,44 @@ class Minimax:
                 best_val = move_val
                 best_move = move
         return best_move
+    
+
+     def check_for_winner(self, state):
+        for row in state:
+            if row[0] == row[1] == row[2] != ' ':
+                return row[0]  
+        for col in range(3):
+            if state[0][col] == state[1][col] == state[2][col] != ' ':
+                return state[0][col] 
+        if state[0][0] == state[1][1] == state[2][2] != ' ':
+            return state[0][0] 
+        if state[0][2] == state[1][1] == state[2][0] != ' ':
+            return state[0][2]  
+        return None  
+
+    def check_for_draw(self, state):
+        return all(cell != ' ' for row in state for cell in row)
+
+    def get_possible_moves(self, state):
+        possible_moves = []
+        for i in range(3):
+            for j in range(3):
+                if state[i][j] == ' ':  
+                    new_state = [row[:] for row in state]  
+                    new_state[i][j] = 'X' 
+                    possible_moves.append(new_state)
+        return possible_moves
+
+if __name__ == "__main__":
+    initial_state = [
+        ['X', 'O', 'X'],
+        [' ', 'O', ' '],
+        [' ', ' ', ' ']
+    ]
+
+    minimax = Minimax(initial_state)
+    best_move = minimax.best_move(initial_state)
+    
+    print("Best move for X:")
+    for row in best_move:
+        print(row)
